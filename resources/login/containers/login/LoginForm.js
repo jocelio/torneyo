7d6ab96b7f4feb8cd4234cf52d7b0ac7c49a0473
Jocelio Lima <jocelio27@gmail.com>
2017-07-26 19:14:38 -0300

    login view and perform login request methodsa and localStorage

--- resources/login/containers/login/LoginForm.js
+++ resources/login/containers/login/LoginForm.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {login} from './actions/actions_login';
+import PropTypes from 'prop-types';
+import {login , storeAuthCredentials } from './actions/actions_login';
 import {styles} from '../../../../public/assets/css/login.css';
 
 class FormLogin extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    handleLogin(){
+        const {username, password} = this.refs;
+
+        this.props.login({username: username.value, password:password.value}).then(response => {
+            if(response.error) throw response.data;
+
+            this.props.storeAuthCredentials(response.payload.data);
+
+            this.context.router.push('/equipe');
+
+        }).catch(error => {
+           alert(error);
+        });
+    }
 
     render(){
         return (<div className="mdl-layout mdl-js-layout mdl-color--grey-100">
@@ -20,17 +40,19 @@ class FormLogin extends Component {
                             <input type="hidden" name="client_id" value="2" />
 
                             <div className="mdl-textfield mdl-js-textfield">
-                                <input className="mdl-textfield__input" type="text" id="username" name="username"/>
+                                <input className="mdl-textfield__input" type="text" id="username" name="username"
+                                ref="username"/>
                                 <label className="mdl-textfield__label" htmlFor="username">Username</label>
                             </div>
                             <div className="mdl-textfield mdl-js-textfield">
-                                <input className="mdl-textfield__input" type="password" id="userpass" name="password" />
+                                <input className="mdl-textfield__input" type="password" id="userpass" name="password"
+                                ref="password"/>
                                 <label className="mdl-textfield__label" htmlFor="userpass">Password</label>
                             </div>
                         </form>
                     </div>
                     <div className="mdl-card__actions mdl-card--border">
-                        <button onClick="submit()" className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" type="submit">Entrar</button>
+                        <button onClick={() => this.handleLogin()} className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" type="submit">Entrar</button>
                     </div>
                 </div>
             </main>
@@ -39,4 +61,12 @@ class FormLogin extends Component {
 
 }
 
-export default connect(null, {login})(FormLogin);
+FormLogin.contextTypes = {
+    router: PropTypes.object
+};
+
+function mapStateToProps(state){
+    return state;
+}
+
+export default connect(mapStateToProps, {login, storeAuthCredentials})(FormLogin);
--- resources/login/containers/login/actions/actions_login.js
+++ resources/login/containers/login/actions/actions_login.js
@@ -2,15 +2,35 @@ import axios from 'axios';
 import {ROOT_URL} from '../../../../app/config'
 
 export const LOGIN = 'LOGIN';
+export const WRITE_LOCALSTORAGE = 'WRITE_LOCALSTORAGE';
 
+export function login(data){
 
-
-
-export function login(equipe){
     const url = `${ROOT_URL}/oauth/token`;
-    const response = axios.get(url);
+
+    const formdata = {
+        client_secret:'bEoyH3MtiBgpLRRgl08wHo2sKra6Me3RuR4IJya0',
+        grant_type:'password',
+        client_id:2,
+        username:data.username ,
+        password:data.password
+    }
+
+    const response = axios.post(url, formdata);
+
     return {
         type: LOGIN,
         payload: response
+    };
+}
+
+export function storeAuthCredentials(credentials){
+
+    localStorage.setItem('access_token',credentials.access_token);
+    localStorage.setItem('expires_in',credentials.expires_in);
+
+    return {
+        type: WRITE_LOCALSTORAGE,
+        payload: true
     };
 }
