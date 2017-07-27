ac1e27f0f8365e0e82d1fe09cbb1543577c36ed9
Jocelio Lima <jocelio.lima@accenture.com>
2017-07-27 17:40:43 -0300

    litle code organization

--- resources/login/containers/login/LoginForm.js
+++ resources/login/containers/login/LoginForm.js
@@ -11,18 +11,20 @@ class FormLogin extends Component {
     }
 
     handleLogin(){
+
         const {username, password} = this.refs;
 
         this.props.login({username: username.value, password:password.value}).then(response => {
+
             if(response.error) throw response.data;
 
             this.props.storeAuthCredentials(response.payload.data);
             this.props.redirectIn()
 
-
         }).catch(error => {
            alert(error);
         });
+
     }
 
     render(){
@@ -30,14 +32,10 @@ class FormLogin extends Component {
             <main className="mdl-layout__content">
                 <div className="mdl-card mdl-shadow--6dp">
                     <div className="mdl-card__title mdl-color--primary mdl-color-text--white">
-                        <h2 className="mdl-card__title-text">Torneyo</h2>
+                        <h2 className="mdl-card__title-text">Login</h2>
                     </div>
                     <div className="mdl-card__supporting-text">
                         <form action="/oauth/token" id="login" method="post">
-
-                            <input type="hidden" name="client_secret" value="bEoyH3MtiBgpLRRgl08wHo2sKra6Me3RuR4IJya0" />
-                            <input type="hidden" name="grant_type" value="password" />
-                            <input type="hidden" name="client_id" value="2" />
 
                             <div className="mdl-textfield mdl-js-textfield">
                                 <input className="mdl-textfield__input" type="text" id="username" name="username"
