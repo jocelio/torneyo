28ee3068f4df5bc66eb72f957bb3133f00e5f6e8
Jocelio Lima <jocelio27@gmail.com>
2017-08-07 20:46:45 -0300

    players seeder

---
+++ database/seeds/PlayerSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PlayerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        \App\Player::truncate();
+
+        \App\Player::insert(array(
+                'name' => 'Neymar',
+                'description' => 'Neymar Junior',
+                'image' => null,
+                'created_at' =>  new DateTime('02/31/2011'),
+                'updated_at' =>  new DateTime('02/31/2011'),
+                'equipe_id' => 1,
+                'due' =>  new DateTime('02/31/2011')
+            ));
+
+
+    }
+}
