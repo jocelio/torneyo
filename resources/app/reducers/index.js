5db54ca2595675a40c82985c504f0603a7a0d9c5
Jocelio Lima <jocelio27@gmail.com>
2017-10-14 15:42:33 -0300

    Redux reg

--- resources/app/reducers/index.js
+++ resources/app/reducers/index.js
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux';
 import EquipeReducer from '../components/equipe/reducers/reducer_equipe';
 import PlayerReducer from '../components/player/reducers/reducer_player';
+import LoginReducer from '../components/login/redux/reducer_login';
+
 import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
     state: (state = {}) => state,
     equipesState: EquipeReducer,
     playersState: PlayerReducer,
+    loginState: LoginReducer,
     form: formReducer
 });
 
