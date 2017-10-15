d27be66696c0c369e571a0da83e22085d698d536
Jocelio Lima <jocelio27@gmail.com>
2017-10-15 13:54:27 -0300

    New menu

--- resources/app/containers/Menu.js
+++ resources/app/containers/Menu.js
@@ -8,6 +8,7 @@ class Menu extends Component {
         <div className="demo-navigation mdl-navigation mdl-color--blue-grey-800 ">
             <MenuItem name="Equipes" href="/equipe" icon="book"/>
             <MenuItem name="Jogadores" href="/player" icon="pets"/>
+            <MenuItem name="Usuários" href="/user" icon="fingerprint"/>
             <MenuItem name="Home" href="/home" icon="home"/>
         </div>
     );
